962e36c38ce9f6c2000916eb9b4cfec7924cf912
AlisaLevchenko <good.luck.odria@gmail.com>
2022-07-05 13:36:31 +0300

    Add code HW#9 part 1

--- src 4/js/01-color-switcher.js
+++ src 4/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+console.dir(startBtn);
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
